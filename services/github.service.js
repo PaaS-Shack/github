e122b1c8e46b582926b582fde5fc48e3dcaea408
FLYBYME <mangoraft@gmail.com>
2023-10-01 14:58:29 -0400

    work

--- services/github.service.js
+++ services/github.service.js
@@ -6,187 +6,418 @@ const { MoleculerClientError } = require("moleculer").Errors;
  * this service manages the github api and webhooks
  * 
  * webhocks used to update paas-shack deployments
+ *  deployment_status,deployment,check_run,workflow,workflow_run,action,repository,organization,sender
  */
 module.exports = {
-    name: "github",
-    version: 1,
+  name: "github",
+  version: 1,
 
-    mixins: [
-        ConfigLoader([
-            'github.**'
-        ]),
-    ],
+  mixins: [
+    ConfigLoader([
+      'github.**'
+    ]),
+  ],
 
+  /**
+   * Service dependencies
+   */
+  dependencies: [
+
+  ],
+
+  /**
+   * Service settings
+   */
+  settings: {
+    rest: "v1/github",
+
+    config: {
+
+    }
+  },
+
+  /**
+   * Actions
+   */
+
+  actions: {
     /**
-     * Service dependencies
+     * github webhook
+     * 
+     * @actions
+     * @param {Object} data - github webhook data
+     * 
+     * @returns {Object} github webhook data
      */
-    dependencies: [
+    webhook: {
+      rest: "POST /webhook",
+      permissions: ['github.webhook'],
+      params: {},
+      async handler(ctx) {
+        const payload = ctx.params;
 
-    ],
+        // payload events
+        const events = this.compactPayload(payload);
 
-    /**
-     * Service settings
-     */
-    settings: {
-        rest: "v1/github",
-
-        config: {
-
+        // loop over events
+        for (const event of events) {
+          // emit event
+          ctx.emit(`github.${event.key}.${event.action}`, event.payload);
+          this.logger.info(`github.${event.key}.${event.action}`, event.payload);
         }
+      }
+    },
+  },
+
+  /**
+   * Events
+   */
+  events: {
+
+  },
+
+  /**
+   * Methods
+   */
+  methods: {
+    /**
+     * compact sender payload
+     * 
+     * @param {Object} payload - sender payload
+     * 
+     * @returns {Object} compacted sender payload
+     */
+    compactSenderPayload(payload) {
+      return {
+        id: payload.id,
+        login: payload.login,
+        node_id: payload.node_id,
+        avatar_url: payload.avatar_url,
+        gravatar_id: payload.gravatar_id,
+        url: payload.url,
+        type: payload.type,
+        site_admin: payload.site_admin
+      };
     },
 
     /**
-     * Actions
+     * compact repository payload
+     * 
+     * @param {Object} payload - repository payload
+     * 
+     * @returns {Object} compacted repository payload
      */
-
-    actions: {
-        /**
-         * github webhook
-         * 
-         * @actions
-         * @param {Object} data - github webhook data
-         * 
-         * @returns {Object} github webhook data
-         */
-        webhook: {
-            rest: "POST /webhook",
-            permissions: ['github.webhook'],
-            params: {},
-            async handler(ctx) {
-                const payload = ctx.params;
-
-                if (payload.registry_package) {
-                    await this.processPackagePublish(ctx, payload);
-                } else {
-
-                    let action = payload.action;
-
-                    if (!action) {
-                        if (payload.forkee) {
-                            action = 'forked';
-                        } else if (payload.ref) {
-                            action = 'pushed';
-                        } else if (payload.release) {
-                            action = 'released';
-                        } else if (payload.pull_request) {
-                            action = 'pull_request';
-                        } else if (payload.issue) {
-                            action = 'issue';
-                        } else if (payload.pages) {
-                            action = 'pages';
-                        } else {
-                            action = 'unknown';
-                        }
-                    }
-                    ctx.emit(`github.actions.${action}`, payload);
-
-                    this.logger.info(`unhandled webhook: ${action} keys: ${Object.keys(payload)}`);
-                }
-            }
-        },
+    compactRepositoryPayload(payload) {
+      return {
+        id: payload.id,
+        node_id: payload.node_id,
+        name: payload.name,
+        full_name: payload.full_name,
+        private: payload.private,
+        owner: this.compactSenderPayload(payload.owner),
+        description: payload.description,
+        fork: payload.fork,
+        url: payload.url,
+        created_at: payload.created_at,
+        updated_at: payload.updated_at,
+        pushed_at: payload.pushed_at,
+        homepage: payload.homepage,
+        size: payload.size,
+        stargazers_count: payload.stargazers_count,
+        watchers_count: payload.watch
+      };
     },
 
     /**
-     * Events
+     * compact deployment payload
+     * 
+     * @param {Object} payload - deployment payload
+     * 
+     * @returns {Object} compacted deployment payload
      */
-    events: {
-
+    compactDeploymentPayload(payload) {
+      return {
+        id: payload.id,
+        node_id: payload.node_id,
+        sha: payload.sha,
+        ref: payload.ref,
+        task: payload.task,
+        payload: payload.payload,
+        environment: payload.environment,
+        description: payload.description,
+        creator: this.compactSenderPayload(payload.creator),
+        created_at: payload.created_at,
+        updated_at: payload.updated_at
+      };
     },
 
     /**
-     * Methods
+     * compact deployment status payload
+     * 
+     * @param {Object} payload - deployment status payload
+     * 
+     * @returns {Object} compacted deployment status payload
      */
-    methods: {
-        /**
-         * process github webhook package publish
-         * 
-         * @param {Object} ctx - moleculer context
-         * @param {Object} payload - github webhook payload
-         */
-        async processPackagePublish(ctx, payload) {
-            const { action, repository, registry_package: pkg } = payload;
-            const { name, package_version } = pkg;
-            if (action === 'published') {
-                this.logger.info(`package published: ${name}@${package_version.version} url: ${package_version.package_url}`);
-                const Package = {
-                    name,
-                    namespace: pkg.namespace,
-                    version: package_version.version.split(':')[1],
-                    url: package_version.package_url,
-                    branch: package_version.target_commitish,
-                    repository: repository.full_name,
-                    registry: "ghcr.io"
-                };
+    compactDeploymentStatusPayload(payload) {
+      return {
+        id: payload.id,
+        node_id: payload.node_id,
+        state: payload.state,
+        creator: this.compactSenderPayload(payload.creator),
+        description: payload.description,
+        created_at: payload.created_at,
+        updated_at: payload.updated_at,
+      };
+    },
 
-                const url = `${Package.registry}/${Package.namespace}/${Package.name}:${Package.branch}`.toLowerCase()
+    /**
+     * compact check run payload
+     * 
+     * @param {Object} payload - check run payload
+     * 
+     * @returns {Object} compacted check run payload
+     */
+    compactCheckRunPayload(payload) {
+      return {
+        id: payload.id,
+        node_id: payload.node_id,
+        head_sha: payload.head_sha,
+        external_id: payload.external_id,
+        url: payload.url,
+        status: payload.status,
+        conclusion: payload.conclusion,
+        started_at: payload.started_at,
+        completed_at: payload.completed_at,
+        output: payload.output,
+        name: payload.name,
+        check_suite: payload.check_suite,
+        app: payload.app,
+        pull_requests: payload.pull_requests
+      };
+    },
 
-                if (package_version.package_url.toLowerCase() == url) {
-                    this.logger.info(`package url: ${package_version.package_url}`);
-                    console.log(Package)
-                    ctx.emit(`github.package.published`, Package);
-                } else {
-                    this.logger.info(`package url not found`);
-                }
-            }
-        },
-        /**
-            * Handle different types of GitHub events and compact the payload
-            * 
-            * @param {string} action - The GitHub action type (e.g., pull_request, release, etc.)
-            * @param {Object} payload - The GitHub webhook payload
-            */
-        handleGitHubEvent(action, payload) {
-            let compactedPayload = {};
+    /**
+     * compact workflow payload
+     * 
+     * @param {Object} payload - workflow payload
+     * 
+     * @returns {Object} compacted workflow payload
+     */
+    compactWorkflowPayload(payload) {
+      return {
+        id: payload.id,
+        node_id: payload.node_id,
+        name: payload.name,
+        path: payload.path,
+        state: payload.state,
+        created_at: payload.created_at,
+        updated_at: payload.updated_at,
+        url: payload.url,
+        html_url: payload.html_url,
+        badge_url: payload.badge_url
+      };
+    },
 
-            if (action === 'pull_request') {
-                compactedPayload = {
-                    action,
-                    pull_request: {
-                        title: payload.pull_request.title,
-                        url: payload.pull_request.url,
-                        html_url: payload.pull_request.html_url,
-                        state: payload.pull_request.state,
-                        merged: payload.pull_request.merged,
-                        draft: payload.pull_request.draft,
-                        head: payload.pull_request.head,
-                        base: payload.pull_request.base,
-                        body: payload.pull_request.body,
-                    }
-                };
-            } else if (action === 'release') {
-                compactedPayload = {
-                    action,
-                    release: {
-                        name: payload.release.name,
-                        tag_name: payload.release.tag_name,
-                        repository: payload.repository.full_name,
-                        url: payload.release.url,
-                        html_url: payload.release.html_url,
-                        assets: payload.release.assets,
-                        body: payload.release.body,
-                        draft: payload.release.draft,
-                    }
-                };
-            }
-            // Add more conditions for other actions as needed...
+    /**
+     * compact workflow run payload
+     * 
+     * @param {Object} payload - workflow run payload
+     * 
+     * @returns {Object} compacted workflow run payload
+     */
+    compactWorkflowRunPayload(payload) {
+      return {
+        id: payload.id,
+        node_id: payload.node_id,
+        head_branch: payload.head_branch,
+        head_sha: payload.head_sha,
+        run_number: payload.run_number,
+        event: payload.event,
+        status: payload.status,
+        conclusion: payload.conclusion,
+        workflow_id: payload.workflow_id,
+        url: payload.url,
+        pull_requests: payload.pull_requests,
+        created_at: payload.created_at,
+        updated_at: payload.updated_at,
+        head_commit: payload.head_commit,
+        repository: this.compactRepositoryPayload(payload.repository),
+        head_repository: this.compactRepositoryPayload(payload.head_repository),
+        sender: this.compactSenderPayload(payload.sender)
+      };
+    },
 
-            // Emit the compacted payload
-            this.ctx.emit(`github.actions.${action}`, compactedPayload);
+    /**
+     * compact action payload
+     * 
+     * @param {Object} payload - action payload
+     * 
+     * @returns {Object} compacted action payload
+     */
+    compactActionPayload(payload) {
+      return {
+        id: payload.id,
+        node_id: payload.node_id,
+        name: payload.name,
+        description: payload.description,
+        identifier: payload.identifier,
+        icon_url: payload.icon_url,
+        created_at: payload.created_at,
+        updated_at: payload.updated_at,
+        permissions: payload.permissions
+      };
+    },
+
+    /**
+     * compact organization payload
+     * 
+     * @param {Object} payload - organization payload
+     * 
+     * @returns {Object} compacted organization payload
+     */
+    compactOrganizationPayload(payload) {
+      return {
+        id: payload.id,
+        node_id: payload.node_id,
+        login: payload.login,
+        url: payload.url,
+        description: payload.description,
+        gravatar_id: payload.gravatar_id,
+        name: payload.name,
+        company: payload.company,
+        blog: payload.blog,
+        location: payload.location,
+        email: payload.email,
+        twitter_username: payload.twitter_username,
+        is_verified: payload.is_verified,
+        has_organization_projects: payload.has_organization_projects,
+        has_repository_projects: payload.has_repository_projects,
+        public_repos: payload.public_repos,
+        public_gists: payload.public_gists,
+        followers: payload.followers,
+        following: payload.following,
+        created_at: payload.created_at,
+        updated_at: payload.updated_at,
+        type: payload.type
+      };
+    },
+
+    /**
+     * payload to events
+     * 
+     * @param {Object} payload - github webhook payload
+     * 
+     * @returns {Array} github action events
+     */
+    compactPayload(payload) {
+      const events = [];
+
+      const action = this.getActionName(payload);
+
+      // loop over payload keys
+      for (const key in payload) {
+        switch (key) {
+          case 'deployment_status':
+            compactedPayload.push({
+              key,
+              action,
+              payload: this.compactDeploymentStatusPayload(payload[key])
+            });
+            break;
+          case 'deployment':
+            compactedPayload.push({
+              key,
+              action,
+              payload: this.compactDeploymentPayload(payload[key])
+            });
+            break;
+          case 'check_run':
+            compactedPayload.push({
+              key,
+              action,
+              payload: this.compactCheckRunPayload(payload[key])
+            });
+            break;
+          case 'workflow':
+            compactedPayload.push({
+              key,
+              action,
+              payload: this.compactWorkflowPayload(payload[key])
+            });
+            break;
+          case 'workflow_run':
+            compactedPayload.push({
+              key,
+              action,
+              payload: this.compactWorkflowRunPayload(payload[key])
+            });
+            break;
+          case 'action':
+            compactedPayload.push({
+              key,
+              action,
+              payload: this.compactActionPayload(payload[key])
+            });
+            break;
+          case 'head_repository':
+            compactedPayload.push({
+              key,
+              action,
+              payload: this.compactRepositoryPayload(payload[key])
+            });
+            break;
+          case 'repository':
+          case 'organization':
+          case 'sender':
+          default:
+            break;
         }
+      }
+
+      return events;
     },
 
     /**
-     * Service created lifecycle event handler
+     * get action name
+     * 
+     * @param {Object} payload - github webhook payload
+     * 
+     * @returns {String} action name
      */
-    created() { },
+    getActionName(payload) {
+      const action = payload.action;
+      let actionName = '';
 
-    /**
-     * Service started lifecycle event handler
-     */
+      if (!action) {
+        actionsName = 'unknown';
+      } else {
+        actionName = action
+      }
+
+      if (actionName == 'unknown') {
+        const payloadKeys = Object.keys(payload);
+        if (payloadKeys.length > 0) {
+          actionName = payloadKeys[0];
+        }
+      }
+
+      return actionName;
+
+    },
+  },
+
+  /**
+   * Service created lifecycle event handler
+   */
+  created() { },
+
+  /**
+   * Service started lifecycle event handler
+   */
 
 
-    /**
-     * Service stopped lifecycle event handler
-     */
-    stopped() { }
+  /**
+   * Service stopped lifecycle event handler
+   */
+  stopped() { }
 };
